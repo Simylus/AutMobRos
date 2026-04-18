9b9f14e0d4d76d55e385cc6575ebd207f960a78f
Simylus <51meon.445m1n@gmail.com>
2026-04-18 11:04:23 +0000

    angular Control

--- inc/ControlSystem.hpp
+++ inc/ControlSystem.hpp
@@ -3,8 +3,11 @@
 
 #include <eeros/control/TimeDomain.hpp>
 #include <eeros/core/Executor.hpp>
+#include <eeros/control/PeripheralInput.hpp>
+#include <eeros/control/SignalChecker.hpp>
 #include <eeros/control/Constant.hpp>
 #include <eeros/control/Gain.hpp>
+#include <eeros/control/PeripheralOutput.hpp>
 
 using namespace eeros::control;
 
@@ -16,6 +19,10 @@ public:
     // Define Blocks
     Constant<> myConstant;
     Gain<> myGain;
+
+    PeripheralInput<> q1;
+    Gain<> g;
+    SignalChecker<> signalChecker;
 
     TimeDomain timedomain;
 };
